690615c1e601aaffdc941b056ae6e2d9334b57ce
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-08-31 13:01:36 +0200

    Add a Calendar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react';
+import { render } from 'react-dom';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const  ReactCalendar = () => {
+    const[date, setDate ]=useState(new Date());
+
+    const onChange = date => {
+        setDate(date);
+    }
+    return (
+      <>
+        <Calendar showWeekNumbers onChange={onChange} value={date}/>
+        {console.log(date)}
+        {date.toString()}
+        
+      </>
+    );
+  }
+  
+ 
+  
+render(<ReactCalendar />, document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
