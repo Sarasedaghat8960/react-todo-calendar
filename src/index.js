d767f56f1dc046e7f82599a1f2bd02f809971a7d
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-08-31 13:11:09 +0200

    change code in index and Calendar

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react'
 import { render } from 'react-dom';
-import Calendar from 'react-calendar';
+import ReactCalendar from './Calendar'
 import 'react-calendar/dist/Calendar.css';
 
 
-const  ReactCalendar = () => {
-    const[date, setDate ]=useState(new Date());
 
-    const onChange = date => {
-        setDate(date);
-    }
-    return (
-      <>
-        <Calendar showWeekNumbers onChange={onChange} value={date}/>
-        {console.log(date)}
-        {date.toString()}
-        
-      </>
-    );
-  }
-  
- 
   
 render(<ReactCalendar />, document.querySelector('#root')
 );
